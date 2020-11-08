c13ddc0c7c07d5f7256e9051ef47d6d4babdb7f7
Javier Andres Pedrozo Payares <pedrozopayares@gmail.com>
2020-11-08 14:46:14 -0500

    StateFulWidget added to app.

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,16 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  DicePage({Key key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 5;
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -27,7 +36,7 @@ class DicePage extends StatelessWidget {
               onPressed: () {
                 print('Left botton got pressed!');
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
